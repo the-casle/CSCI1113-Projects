fca3e36dfbc8805326ddd553135813a9c0bea2e1
the-casle <redeyedtreefrog28@gmail.com>
2020-02-04 22:07:27 -0600

    Initial Projects

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Jake Kaslewicz on 1/28/20.
+//  Copyright © 2020 Jake Kaslewicz. All rights reserved.
+//
+
+#import <iostream>
+using namespace std;
+
+void yoyeet(int arg1){
+    cout << "pen\n";
+}
+
+int main(int argc, const char * argv[]) {
+    string yeet = "nice\n";
+    unsigned long yayeet = 69;
+    cout << yayeet << endl << yeet;
+    return 0;
+}
+
---
+++ PartyHat/PartyHat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PartyHat
+//
+//  Created by Jake Kaslewicz on 2/4/20.
+//  Copyright © 2020 Jake Kaslewicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
