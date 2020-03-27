fc9f6983acb7fd683765a187249e2c2b6fc6b47a
the-casle <redeyedtreefrog28@gmail.com>
2020-03-27 11:03:43 -0500

    lab

--- Warm Up week 8/Warm Up week 8/main.cpp
+++ Warm Up week 8/Warm Up week 8/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+using namespace std;
+
+void reverseNum(int n);
+int maxValue( int vals[], int size, int start );
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    reverseNum(5786);
+    int array[5] = {55,3,77,2,56};
+    cout << maxValue(array, 5, 0) << endl;
     return 0;
+}
+
+void reverseNum(int n){
+    if(n < 10) cout << n << endl;
+    else {
+        cout << n % 10;
+        reverseNum(n / 10);
+    }
+}
+
+int maxValue(int vals[], int size, int start ){
+    if(size == 1) return vals[start];
+    else {
+        if(vals[start] > vals[start + size - 1]){
+            return maxValue(vals, size - 1, start);
+        } else {
+            return maxValue(vals, size - 1, start + 1);
+        }
+    }
 }
